2c9d4408a45dd52fa1451076aa2e11e64955f27d
Jeff88Ho <stone40licks@gmail.com>
2015-12-11 22:14:17 -0500

    update

--- src/main/scala/GoldenRatio.scala
+++ src/main/scala/GoldenRatio.scala
@@ -6,10 +6,6 @@ import math.pow
 
 object GoldenRatio extends App {
 
-
-  // Change parameters here!
-  calculate(nrOfWorkers = 10, nrOfElements = 5, nrOfMessages = 10)
-
   sealed trait GRMessage
   case object Calculate extends GRMessage
   case class Work(start: Int, nrOfElements: Int) extends GRMessage
@@ -24,64 +20,48 @@ object GoldenRatio extends App {
         case _ => n * factorial(n-1)
       }
 
-      (start until (start + nrOfElements))
-        .map( x => pow(-1, x+1) * factorial(2*x+1) / factorial(x+2) / factorial(x) / pow(4, 2*x+3)).sum
+      (start until (start+nrOfElements)).map( x => pow(-1, x+1) * factorial(2*x+1) / factorial(x+2) / factorial(x) / pow(4, 2*x+3)).sum
     }
 
     def receive = {
-      case Work(start, nrOfElements) ⇒
-        sender ! Result(calculateGRFor(start, nrOfElements)) // perform the work
+      case Work(start, nrOfElements) => sender ! Result(calculateGRFor(start, nrOfElements))
     }
   }
 
-  class Master(nrOfWorkers: Int, nrOfMessages: Int, nrOfElements: Int, listener: ActorRef)
-    extends Actor {
-
+  class Master(nrOfWorkers: Int, nrOfMessages: Int, nrOfElements: Int, listener: ActorRef) extends Actor {
     var gr: Double = 13.0 / 8.0
     var nrOfResults: Int = _
     val start: Long = System.currentTimeMillis
-
-    val workerRouter = context.actorOf(
-      Props[Worker].withRouter(RoundRobinPool(nrOfWorkers)), name = "workerRouter")
+    val workerRouter = context.actorOf(Props[Worker].withRouter(RoundRobinPool(nrOfWorkers)), name = "workerRouter")
 
     def receive = {
-      case Calculate ⇒
-        for (i ← 0 until nrOfMessages) workerRouter ! Work(i * nrOfElements, nrOfElements)
-      case Result(value) ⇒
+      case Calculate => for (i ← 0 until nrOfMessages) workerRouter ! Work(i * nrOfElements, nrOfElements)
+      case Result(value) =>
         gr += value
         nrOfResults += 1
         if (nrOfResults == nrOfMessages) {
-          // Send the result to the listener
           listener ! GRApproximation(gr, duration = (System.currentTimeMillis - start).millis)
-          // Stops this actor and all its supervised children
           context.stop(self)
         }
     }
-
   }
 
   class Listener extends Actor {
     def receive = {
-      case GRApproximation(gr, duration) ⇒
-        println("\n\tGR approximation: \t\t%s\n\tCalculation time: \t%s"
-          .format(gr, duration))
+      case GRApproximation(gr, duration) =>
+        println("\n\tGR approximation: \t\t%s\n\tCalculation time: \t%s".format(gr, duration))
         context.system.terminate()
     }
   }
 
   def calculate(nrOfWorkers: Int, nrOfElements: Int, nrOfMessages: Int) {
-    // Create an Akka system
     val system = ActorSystem("GRSystem")
-
-    // create the result listener, which will print the result and shutdown the system
     val listener = system.actorOf(Props[Listener], name = "listener")
+    val master = system.actorOf(Props(new Master(nrOfWorkers, nrOfMessages, nrOfElements, listener)), name = "master")
 
-    // create the master
-    val master = system.actorOf(Props(new Master(nrOfWorkers, nrOfMessages, nrOfElements, listener)),
-      name = "master")
-
-    // start the calculation
     master ! Calculate
-
   }
+
+  // Change parameters here!
+  calculate(nrOfWorkers = 10, nrOfElements = 5, nrOfMessages = 10)
 }
